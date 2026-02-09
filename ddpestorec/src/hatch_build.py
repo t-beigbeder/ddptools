6238e1446fe40442db417ede328c9841682dc995
thierry_otvl <tbeigbeder.nospam@gmail.com>
2026-02-09 15:06:49 +0000

    clu,fx

--- ddpestorec/src/hatch_build.py
+++ ddpestorec/src/hatch_build.py
@@ -9,6 +9,7 @@ class MetaDataHook(MetadataHookInterface):
         v = os.getenv("V_DDPT_V")
         if not v:
             raise ValueError("V_DDPT_V environment variable shoud be set before building this package")
+        metadata["version"] = v
         metadata["dependencies"] = []
         with pathlib.Path(self.root, "requirements.txt").open() as if_:
             for ln in if_:
