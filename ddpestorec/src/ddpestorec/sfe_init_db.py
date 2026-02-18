9890e09e55a6e277cd42043e25d3f3abd28dac08
thierry_otvl <tbeigbeder.nospam@gmail.com>
2026-02-18 08:04:37 +0000

    refacto main and added drop option

--- ddpestorec/src/ddpestorec/sfe_init_db.py
+++ ddpestorec/src/ddpestorec/sfe_init_db.py
@@ -12,25 +12,28 @@ from . import main
 logger = logging.getLogger("sfe_init_db")
 
 
-def run(host: str, port: str, drop: bool, category: str):
-    logger.info(f"main: calling CreateRequest on {host}:{port} drop {drop}")
+def run(host: str, port: str, drop: bool):
+    logger.info(f"run: calling CreateRequest on {host}:{port} drop {drop}")
     with grpc.insecure_channel(f"{host}:{port}") as channel:
         stub = estore_pb2_grpc.EstoreStub(channel)
         stub.Create(
             estore_pb2.CreateRequest(exist_ok=True, drop=drop, ddl="")
         )
-    logger.info("main: exiting")
+    logger.info("run: exiting")
 
 
 def _parser(parser: argparse.ArgumentParser) -> None:
-    drop = True if os.getenv("ESTORE_DROP_DB", "0").lower() not in ("", "0", "false") else False
+    parser.add_argument("--drop", action=argparse.BooleanOptionalAction, default=False)
 
 
 def _doer(
     _: logging.Logger, host: str, port: str, args: argparse.Namespace
 ) -> int:
-    drop = True if os.getenv("ESTORE_DROP_DB", "0").lower() not in ("", "0", "false") else False
-    run(host, port, drop, args.category)
+    if os.getenv("ESTORE_DROP_DB") is None:
+        drop = args.drop
+    else:
+        drop = True if os.getenv("ESTORE_DROP_DB", "0").lower() not in ("", "0", "false") else False
+    run(host, port, drop)
     return 0
 
 
--- ddpestorec/src/ddpestorec/sfe_ope_cli.py
+++ ddpestorec/src/ddpestorec/sfe_ope_cli.py
@@ -1,44 +1,43 @@
 import argparse
 import logging
-import os
 import sys
 
 import grpc
 
 from sfegrpc import ope_pb2_grpc, ope_pb2
+from ddpestorec import main
 
 
 logger = logging.getLogger("sfe_ope_cli")
 
 
-def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--host")
-    parser.add_argument("-p", "--port")
-    parser.add_argument("--ready", action=argparse.BooleanOptionalAction)
-    parser.add_argument("--shutdown")
-    parser.add_argument("--version", action=argparse.BooleanOptionalAction)
-    args = parser.parse_args()
-    host = args.host if args.host else "localhost"
-    port = args.port if args.port else "8080"
-    logging.basicConfig(
-        format="%(asctime)s %(name)s %(levelname)-8s %(message)s",
-        datefmt="%Y-%m-%d %H:%M:%S",
-        stream=sys.stderr,
-    )
-    logger.setLevel(os.getenv("LOG_LEVEL", "INFO"))
+def run(host: str, port: str, ready: bool, shutdown: str | None, version: bool) -> None:
     with grpc.insecure_channel(f"{host}:{port}") as channel:
         stub = ope_pb2_grpc.OpeStub(channel)
-        if args.ready:
+        if ready:
             r_resp = stub.Ready(ope_pb2.Empty())
             logger.info(f"main: Ready {r_resp.value}")
-        elif args.shutdown:
-            s_resp = stub.Shutdown(ope_pb2.Value(value=args.shutdown))
+        elif shutdown:
+            s_resp = stub.Shutdown(ope_pb2.Value(value=shutdown))
             logger.info(f"main: Shutdown {s_resp.value}")
-        elif args.version:
+        elif version:
             v_resp = stub.Version(ope_pb2.Empty())
             logger.info(f"main: Version {v_resp.value}")
 
 
+def _parser(parser: argparse.ArgumentParser) -> None:
+    parser.add_argument("--ready", action=argparse.BooleanOptionalAction)
+    parser.add_argument("--shutdown")
+    parser.add_argument("--version", action=argparse.BooleanOptionalAction)
+
+
+def _doer(
+    _: logging.Logger, host: str, port: str, args: argparse.Namespace
+) -> int:
+    run(host, port, args.ready, args.shutdown, args.version)
+    return 0
+
+
 if __name__ == "__main__":
-    main()
+    status = main.main(logger, _parser, _doer)
+    sys.exit(status)
